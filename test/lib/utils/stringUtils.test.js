6a864f9cae85a81d9f0333287719b345b23fb49d
alhazen11 <alzahid.m@tokopedia.com>
2021-10-03 16:59:43 +0700

    add describe

--- test/lib/utils/stringUtils.test.js
+++ test/lib/utils/stringUtils.test.js
@@ -1,21 +1,24 @@
 const { stringUtils } = require('@generate/lib/utils');
 
-test('check string contains space', () => {
-    let stringTest = "new project"
-    return expect(stringUtils.containsSpace(stringTest)).toBe(true)
-});
+describe('StringUtils', () => {
 
-test('check string contains not space', () => {
-    let stringTest = "adonis"
-    return expect(stringUtils.containsSpace(stringTest)).toBe(false)
-});
+    test('check string contains space', () => {
+        let stringTest = "new project"
+        return expect(stringUtils.containsSpace(stringTest)).toBe(true)
+    });
 
-test('check string contains symbol', () => {
-    let stringTest = "project?"
-    return expect(stringUtils.containSymbol(stringTest)).toBe(true)
-});
+    test('check string contains not space', () => {
+        let stringTest = "adonis"
+        return expect(stringUtils.containsSpace(stringTest)).toBe(false)
+    });
 
-test('check string contains not symbol', () => {
-    let stringTest = "project"
-    return expect(stringUtils.containSymbol(stringTest)).toBe(false)
+    test('check string contains symbol', () => {
+        let stringTest = "project?"
+        return expect(stringUtils.containSymbol(stringTest)).toBe(true)
+    });
+
+    test('check string contains not symbol', () => {
+        let stringTest = "project"
+        return expect(stringUtils.containSymbol(stringTest)).toBe(false)
+    });
 });
